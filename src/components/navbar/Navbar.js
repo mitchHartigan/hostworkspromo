f44071c4106e361ef4a6d2c7ed16944f8b8a1acb
Mitchell Hartigan <mitch.hartigan@gmail.com>
2022-06-02 10:16:24 -0700

    feat: searchbar link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { SearchBarLink } from "./SearchBarLink";
 
 import { TextLink } from "./TextLink";
 
@@ -18,6 +19,7 @@ export const Navbar = (props) => {
           <TextLink destination="https://www.hostworkspromo.com/contact.htm">
             Contact
           </TextLink>
+          <SearchBarLink />
         </LinkContainer>
       </Bar>
     </Container>
@@ -49,6 +51,7 @@ const LinkContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  align-items: center;
   width: 45%;
   margin-top: -3px;
 `;
