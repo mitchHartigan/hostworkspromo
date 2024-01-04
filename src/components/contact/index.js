2a9b3bb84bc5bbcace0d8dccc5154906860bd2e9
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2024-01-03 16:31:20 -0800

    feat: fade in contact form

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,23 +1,37 @@
-import { Title } from "components/Title";
 import React, { useState } from "react";
+import { Title } from "components/Title";
+import VisibilitySensor from "react-visibility-sensor";
+import { FadeIn } from "../FadeIn";
 import styled from "styled-components";
 
 import Form from "./Form";
 
 export default function Contact(props) {
   const { interest, refProp } = props;
+  const [visible, setVisible] = useState(false);
 
   return (
-    <Container ref={refProp}>
-      <Title
-        containerStyles="margin-bottom: 40px;"
-        align="center"
-        spanWidth="120px"
-      >
-        Contact Us
-      </Title>
-      <Form interest={interest} />
-    </Container>
+    <VisibilitySensor
+      partialVisibility={true}
+      onChange={(visible) => {
+        if (visible) setVisible(true);
+      }}
+    >
+      <Container ref={refProp}>
+        <FadeIn delay="0" visible={visible}>
+          <Title
+            containerStyles="margin-bottom: 40px;"
+            align="center"
+            spanWidth="120px"
+          >
+            Contact Us
+          </Title>
+        </FadeIn>
+        <FadeIn delay="300" visible={visible}>
+          <Form interest={interest} />
+        </FadeIn>
+      </Container>
+    </VisibilitySensor>
   );
 }
 
