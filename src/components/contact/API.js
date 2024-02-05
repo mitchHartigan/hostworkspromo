0723bfdfa70c7749c8a2ffec8218f5dd2a65627a
Mitch <42709932+mitchHartigan@users.noreply.github.com>
2024-02-04 22:07:49 -0800

    migration: print env variable on submit

--- src/components/contact/API.js
+++ src/components/contact/API.js
@@ -1,6 +1,27 @@
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const POST_CONTACT_FORM = async (name, email, phone, message) => {
-  await sleep(3000);
+  console.log('key', process.env.REACT_APP_WEB3FORMS_ACCESS_KEY)
   return true;
+
+  // const url = "https://api.web3forms.com/submit";
+  // const formData = {
+  //   name,
+  //   email,
+  //   phone,
+  //   message,
+  //   subject: "New submission from Hostworks Promo",
+  //   access_key: process.env.REACT_APP_WEB3FORMS_ACCESS_KEY,
+  // }
+
+  // let result = await fetch(url, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Accept": "application/json"
+  //   },
+  //   body: JSON.stringify(formData),
+  // })
+
+  // result = await result.json()
+  // console.log('result', result)
+  // if (result.success) return true;
 };
