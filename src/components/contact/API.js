64d4bf35486bd0f845bcf70b99a6107a547581a4
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2024-02-07 15:59:06 -0800

    deploy: link new form backend service

--- src/components/contact/API.js
+++ src/components/contact/API.js
@@ -1,27 +1,24 @@
 export const POST_CONTACT_FORM = async (name, email, phone, message) => {
-  console.log('key', process.env.REACT_APP_WEB3FORMS_ACCESS_KEY)
-  return true;
+  const url = "https://api.web3forms.com/submit";
+  const formData = {
+    name,
+    email,
+    phone,
+    message,
+    subject: "New submission from Hostworks Promo",
+    access_key: process.env.REACT_APP_WEB3FORMS_ACCESS_KEY,
+  };
 
-  // const url = "https://api.web3forms.com/submit";
-  // const formData = {
-  //   name,
-  //   email,
-  //   phone,
-  //   message,
-  //   subject: "New submission from Hostworks Promo",
-  //   access_key: process.env.REACT_APP_WEB3FORMS_ACCESS_KEY,
-  // }
+  let result = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
 
-  // let result = await fetch(url, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "Accept": "application/json"
-  //   },
-  //   body: JSON.stringify(formData),
-  // })
-
-  // result = await result.json()
-  // console.log('result', result)
-  // if (result.success) return true;
+  result = await result.json();
+  console.log("result", result);
+  if (result.success) return true;
 };
